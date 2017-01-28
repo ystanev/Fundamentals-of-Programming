e32b8da420a8200a6ed2becc14eaf95e61cf3fb3
Yury <ystanev@yahoo.ca>
2017-01-28 12:07:02 -0500

    Update a2_q1.cpp

--- assignment_2/a2_q1.cpp
+++ assignment_2/a2_q1.cpp
@@ -3,41 +3,44 @@
 #include <math.h> // Used for sqrt().
 using namespace std;// ?
 
-//Functioncions Prototype
+//Functions Prototype
 double inputTime();
 double inputGravity();
-double fallingDistance (double time, double gravity);
-void printResult(double time, double gravity, double distance);
+double fallingDistance (double *time, double *gravity);
+void printResult(double *time, double *gravity, double *distance);
 
 int main(){
+
+    //Variable
+    double time_1, gravity_1, distance_1;
 
     // Calling the functions.
     inputTime();
     inputGravity();
-    fallingDistance(inputTime(), inputGravity());
-    printResult(inputTime(), inputGravity(), fallingDistance(inputTime(), inputGravity()));
+    fallingDistance(&time_1, &gravity_1);
+    printResult(&time_1, &gravity_1, &distance_1);
 
 }
 
 double inputTime(){
     double time;
-    cout << "Enter, how the long the object will be falling (sec): " << endl;
+    cout << "Enter, how the long the object will be falling (sec): ";
     cin >> time;
     return time;
 }
 
 double inputGravity(){
     double gravity;
-    cout << "Enter, acceletation due to gravity (m/sec^2): " << endl;
+    cout << "Enter, acceletation due to gravity (m/sec^2): ";
     cin >> gravity;
     return gravity;
 }
 
-double fallingDistance (double time, double gravity){
-    double distance = (1/2) * gravity * pow(time, 2); // Calculates the falling distance & assigns it to a variable.
+double fallingDistance (double *time, double *gravity){
+    double distance = (1/2) * (*gravity) * pow((*time), 2); // Calculates the falling distance & assigns it to a variable.
     return distance;
 }
 
-void printResult(double time, double gravity, double distance){
-    cout << "The time is, " << time << " the gravity is, " << gravity << " there for the falling distance is, " << distance << endl;
+void printResult(double *time, double *gravity, double *distance){
+    cout << "The time is, " << *time << " the gravity is, " << *gravity << " there for the falling distance is, " << *distance << endl;
 }
