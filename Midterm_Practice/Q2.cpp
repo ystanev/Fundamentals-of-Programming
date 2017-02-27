4ed78a79f63aad3b1096aeda456e25f83afed13b
Yury <ystanev@yahoo.ca>
2017-02-26 21:18:24 -0500

    Update Q2.cpp

--- Midterm_Practice/Q2.cpp
+++ Midterm_Practice/Q2.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <cstdlib>
-#include <time.h>       
+#include <time.h>
+#include <algorithm> //Used for sort()
 using namespace std;
 
-//Function Prototype             
-void bubbleSort(int nums[], int SIZE);
+//Function Prototype
+//void bubbleSort(int nums[], int SIZE);
 void distinct_numbers (int nums[], int SIZE);
-			 
-int main (){  
-	//Random Nums.
-	srand(time(NULL));
-			
-	const int SIZE = 100;
-	int nums[SIZE];
-			
-	for (int i=0; i<SIZE; i++) {
-		nums[i] = rand() % 100 + 0; //Generate rand. nums. 0-100   		
-		cout << nums[i] << " ";
-	}
-		
-	//Function Call
-	bubbleSort(nums, SIZE); //Sort array using Bubble Sort
-	distinct_numbers(nums, SIZE);
-	
+
+int main (){
+    //Random Nums.
+    srand(time(NULL));
+
+    const int SIZE = 100;
+    int nums[SIZE];
+
+    for (int i=0; i<SIZE; i++) {
+        nums[i] = rand() % 100 + 0; //Generate rand. nums. 0-100
+        cout << nums[i] << " ";
+    }
+
+    //Function Call
+    //bubbleSort(nums, SIZE); //Sort array using Bubble Sort
+    sort(begin(nums), end(nums));
+    distinct_numbers(nums, SIZE);
+
 }
-				
-void bubbleSort(int nums[], int SIZE){
+
+/*void bubbleSort(int nums[], int SIZE){
     bool swapped = true;
     int j = 0;
     int tmp;
@@ -41,17 +43,17 @@ void bubbleSort(int nums[], int SIZE){
             }
         }
     }
-}
+}*/
 
 void distinct_numbers (int nums[], int SIZE){
     //Checking for duplicates.
-	int dup_count = 0;
-	int uni_num;
+    int dup_count = 0;
+    int uni_num;
     for (int i=1; i<SIZE; i++){
         if(nums[i] == nums[i+1]){
-			dup_count++;//Counts the number of duplicates.	
-			uni_num = SIZE - dup_count;	
+            dup_count++;//Counts the number of duplicates.
+            uni_num = SIZE - dup_count;
         }
     }
-	cout << "\n" << "The # of distinct integers between 0 and 100 = " << uni_num;
+    cout << "\n" << "The # of distinct integers between 0 and 100 = " << uni_num;
 }
