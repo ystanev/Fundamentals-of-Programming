993366fdf1b1e91b5038414866ac26197f3b706d
Yury <ystanev@yahoo.ca>
2017-02-27 20:25:55 -0500

    Update Q7.cpp

--- Midterm_Practice/Q7.cpp
+++ Midterm_Practice/Q7.cpp
@@ -5,15 +5,16 @@ using namespace std;
 int main() {
 
     int h, count = 0;
+	int nums[12] = {1,2,3,4,5,6,7,8,9,10,12};
 
     for (int i=1; i<=3000; i++){
         h= i * ( 2 * i -1);
-//        cout << h << " ";
-
-        if(h%1==0 && h%2==0 && h%3==0 && h%4==0 && h%5==0 && h%6==0 
-           && h%7==0 && h%8==0 && h%9==0 && h%10==0 && h%11==0 && h%12==0) {
-           count ++;
-        }
+		
+		for(int j=0; j<12; j++){
+			if(h%nums[j]==0){
+				count++;
+			}			
+		}
     }
 
     cout << "Of the first 3000 hexagonal #'s " << count << " are divisble by integers form 1 to 12.";
