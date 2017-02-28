421407377f732858b3ef178efc6fc70098e3c489
Yury <ystanev@yahoo.ca>
2017-02-27 20:13:31 -0500

    Create Q7.cpp

---
+++ Midterm_Practice/Q7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+
+    int h, count = 0;
+
+    for (int i=1; i<=3000; i++){
+        h= i * ( 2 * i -1);
+//        cout << h << " ";
+
+        if(h%1==0 && h%2==0 && h%3==0 && h%4==0 && h%5==0 && h%6==0 
+           && h%7==0 && h%8==0 && h%9==0 && h%10==0 && h%11==0 && h%12==0) {
+           count ++;
+        }
+    }
+
+    cout << "Of the first 3000 hexagonal #'s " << count << " are divisble by integers form 1 to 12.";
+
+    system("PAUSE");
+    return 0;
+}
