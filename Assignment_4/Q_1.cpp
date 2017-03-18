b376724412a563b2faf32370413711f5b9a3a005
Yury <ystanev@yahoo.ca>
2017-03-17 21:59:44 -0400

    Add files via upload

--- Assignment_4/Q_1.cpp
+++ Assignment_4/Q_1.cpp
@@ -7,29 +7,37 @@ using namespace std;
 
 int main(){
 
-    Clock clock_1; // Create clock_1 object
+    Clock clock_1; // Create clock_1 object Default Constructor
 
     int h_1, m_1, s_1; // Variables for the user input
 
     //User Input
     cout << "Enter hours: ";
     cin >> h_1;
-    clock_1.incrementHours(h_1); //Increment by value entered by the user
+    //clock_1.incrementHours(h_1); //Increment by value entered by the user
 
     cout << "Enter minutes: ";
     cin >> m_1;
-    clock_1.incrementMins(m_1);
+    //clock_1.incrementMins(m_1);
 
     cout << "Enter seconds: ";
     cin >> s_1;
-    clock_1.incrementSecs(s_1);
+    //clock_1.incrementSecs(s_1);
 
     clock_1.setClock(h_1, m_1, s_1); // Setter
+        clock_1.incrementHours(); //Regular Functions
+        clock_1.incrementMins();
+        clock_1.incrementSecs();
     clock_1.getClock(&h_1, &m_1, &s_1); // Getter
 
-    Clock clock_2 (h_1, m_1, s_1);
+    cout << "The time is: " << h_1 << ":" << m_1 << ":" << s_1 << endl;
+
+    Clock clock_2 (h_1, m_1, s_1); // Overloaded Constructor
 
     clock_2.setClock(h_1, m_1, s_1);
+        clock_2.incrementHours(h_1); //Overloaded Function
+        clock_2.incrementMins(m_1);
+        clock_2.incrementSecs(s_1);
     clock_2.getClock(&h_1, &m_1, &s_1);
 
     cout << "The time is: " << h_1 << ":" << m_1 << ":" << s_1 << endl;
